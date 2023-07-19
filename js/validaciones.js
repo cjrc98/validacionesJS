c0d1e07586feacfa29b5583e97ff338d3f1fb597
camilo <camilored30@gmail.com>
2023-07-19 15:48:25 -0500

    se cargan validaciones de formulario con javascript

---
+++ js/validaciones.js
@@ -0,0 +1,90 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    }else{
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+  ];
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacio"
+    },
+    email: {
+        valueMissing: "El campo email no puede estar vacio",
+        typeMismatch: "El correo no es valido",
+    },
+    password: {
+        valueMissing: "La contraseña no puede estar vacia",
+        patternMismatch: "Al menos 6 carateres, maximo 12, debe contener una minuscula, una mayuscula, un número, y no debe contener caracteres especiales"
+    },
+    nacimiento: {
+        valueMissing: "Este campo no puede estar vacio",
+        customError: "Debes ser mayor de edad para poder registrarte :(" 
+    },
+    numero: {
+        valueMissing: "Este campo no puede estar vacío",
+        patternMismatch: "El formato requerido es XXXXXXXXXX 10 números",
+    },
+    direccion: {
+        valueMissing: "Este campo no puede estar vacío",
+        patternMismatch: "La dirección debe contener entre 10 a 40 caracteres.",
+    },
+    ciudad: {
+        valueMissing: "Este campo no puede estar vacío",
+        patternMismatch: "La ciudad debe contener entre 10 a 40 caracteres.",
+    },
+    estado: {
+        valueMissing: "Este campo no puede estar vacío",
+        patternMismatch: "El estado debe contener entre 10 a 40 caracteres.",
+    },
+};
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+      if (input.validity[error]) {
+        console.log(tipoDeInput, error);
+        console.log(input.validity[error]);
+        console.log(mensajesDeError[tipoDeInput][error]);
+        mensaje = mensajesDeError[tipoDeInput][error];
+      }
+    });
+    return mensaje;
+};
+
+const validadores = {
+    nacimiento: (input) => validarNacimiento(input),
+};
+
+function validarNacimiento(input) {
+    const fecha = new Date(input.value);
+    let mensaje = ""
+    if( !mayorDeEdad(fecha) ){
+        mensaje = "Debes ser mayor de edad para poder registrarte :(";
+    }
+    input.setCustomValidity(mensaje);
+}
+
+function mayorDeEdad(fecha) {
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(
+        fecha.getUTCFullYear() + 18,
+        fecha.getUTCMonth(),
+        fecha.getUTCDate()
+    );
+    return diferenciaFechas < fechaActual;
+}
